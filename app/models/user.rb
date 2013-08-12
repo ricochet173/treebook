0f34646a183d979a1aef80ff32c0c3efb2c07825
Steve Addona <steve.addona@yahoo.com>
2013-08-12 03:54:33 +0000

    Added gravatar functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,15 @@ class User < ActiveRecord::Base
   
   has_many :statuses
   
-   def full_name
+  def full_name
     first_name + " " + last_name
+  end
+  
+  def gravatar_url
+    stripped_email = email.strip
+    downcased_email = stripped_email.downcase
+    hash = Digest::MD5.hexdigest(downcased_email)
+    
+    "http://gravatar.com/avatar/#{hash}"
   end
 end
